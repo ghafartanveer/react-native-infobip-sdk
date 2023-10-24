a04c65797128d91d50b6a3857bfac91d595a0c45
DevHumzaAhzam <105771333+DevHumzaAhzam@users.noreply.github.com>
2023-10-24 18:31:09 +0500

    updated initial code

---
+++ ios/AwesomeModule.swift
@@ -0,0 +1,12 @@
+@objc(AwesomeModule)
+class AwesomeModule: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+   @objc(add:withB:withResolver:withRejecter:)
+  func add(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a+b)
+  }
+}
